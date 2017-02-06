6fef9b6553cd25cd3924fdbb2fdec28549b5fbde
Dodo <donovan.persent@gmail.com>
2017-02-06 10:56:09 +0100

    Generate home and users

--- V0.USers/config/routes.rb
+++ V0.USers/config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  root 'home#index'
+
+  #retour à l'acceuil
+  get 'home/index'
+  get 'home/show'
+
+  resources :users
+  get 'sessions/new'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

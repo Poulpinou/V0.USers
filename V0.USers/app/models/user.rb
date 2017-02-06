bc715e0589f2510e32f802aca493ea0edb45721c
Dodo <donovan.persent@gmail.com>
2017-02-06 12:24:51 +0100

    Affichage WIP

--- V0.USers/app/models/user.rb
+++ V0.USers/app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :pseudo, :presence => true
+  validates :password, :presence => true
+  validates :role, :presence => true
 end
--- V0.USers/db/seeds.rb
+++ V0.USers/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!([
+  {pseudo: "Dodo", password: "dodorider", picture: "http://1.bp.blogspot.com/-EbalO4vytNo/T0E_eJnWfsI/AAAAAAAABB0/ukRH3LUD2kY/s1600/dodo+after+michael+kutsche+19+feb+2012.JPG", role: "Admin"}
+])

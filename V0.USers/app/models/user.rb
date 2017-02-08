3335b41515524d68b5e1a38e7e898bd872d6046c
Dodo <donovan.persent@gmail.com>
2017-02-08 11:15:04 +0100

    First implement Devise and coreect bcrypt

--- V0.USers/app/models/user.rb
+++ V0.USers/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-  validates :pseudo, :presence => true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  #validates :pseudo, :presence => true
   validates :password, :presence => true
   validates :role, :presence => true
   has_many :idea
--- V0.USers/db/schema.rb
+++ V0.USers/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170206144202) do
+ActiveRecord::Schema.define(version: 20170208094341) do
 
   create_table "ideas", force: :cascade do |t|
     t.string   "title"
@@ -26,10 +26,22 @@ ActiveRecord::Schema.define(version: 20170206144202) do
   create_table "users", force: :cascade do |t|
     t.string   "pseudo"
     t.string   "password"
-    t.string   "picture",    default: "http://istc-pc-test-media.cs.washington.edu/images/default-profile-pic.png"
-    t.string   "role",       default: "User"
-    t.datetime "created_at",                                                                                        null: false
-    t.datetime "updated_at",                                                                                        null: false
+    t.string   "picture",                default: "http://istc-pc-test-media.cs.washington.edu/images/default-profile-pic.png"
+    t.string   "role",                   default: "User"
+    t.datetime "created_at",                                                                                                    null: false
+    t.datetime "updated_at",                                                                                                    null: false
+    t.string   "email",                  default: "",                                                                           null: false
+    t.string   "encrypted_password",     default: "",                                                                           null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,                                                                            null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end

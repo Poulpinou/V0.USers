3475d04eee134ca6b915796f92b9d7db1ab7fd01
Dodo <donovan.persent@gmail.com>
2017-02-08 11:51:31 +0100

    Fully functionnal user login system + update call of new_posy

--- V0.USers/app/models/user.rb
+++ V0.USers/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  #validates :pseudo, :presence => true
+  validates :pseudo, :presence => true
   validates :password, :presence => true
   validates :role, :presence => true
   has_many :idea

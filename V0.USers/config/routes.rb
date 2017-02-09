af60c0294bca95e76e9acc8d7c5d55c5d2769de3
Dodo <donovan.persent@gmail.com>
2017-02-09 16:56:27 +0100

    Sort Ideas Okay

--- V0.USers/config/routes.rb
+++ V0.USers/config/routes.rb
@@ -18,5 +18,8 @@ Rails.application.routes.draw do
   #Manual update (temporary)
   get 'update/ranks', to: "application#update_ranks", as: "update_ranks"
 
+  # Sort drop down
+  get 'ideas/sort/:type', to: "ideas#sort_type", as: "sort_ideas"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

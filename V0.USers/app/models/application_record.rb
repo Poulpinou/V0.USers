bebfe4f292da9a11123777df5d75f0c97d038cc4
Poulpinou <donovan.persent@gmail.com>
2017-02-25 16:52:45 +0100

    Basic Css for Home

--- V0.USers/app/models/application_record.rb
+++ V0.USers/app/models/application_record.rb
@@ -1,8 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
-  def ago
-    time = DateTime.now.to_i - self.created_at.to_i
+  def ago date = self.created_at.to_i
+    time = DateTime.now.to_i - date.to_i
     return "#{time} seconds ago" if time < 60
     time = (time/60).to_i
     return "#{time} minutes ago" if time < 60
